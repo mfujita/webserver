cfc8c2b5b38fd0e04f17f061f32564ee56139cb3
Murilo Fujita <murilofujita@gmail.com>
2024-06-17 12:30:34 -0300

    teclog tme fancyglam landind page

---
+++ fancyglam/landingpage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="landingpage.css">
+    <title>Landing Page</title>
+</head>
+<body>
+    <div class="tela1">
+        <div class="Tela1Esquerda">
+            <div class="Tela1titulo">REALCE SUA BELEZA</div>
+            <div class="Tela1texto">A Fancy Glam é um empresa que veio para revolucionar no ramo da beleza.</div>
+            <div class="Tela1texto">Cuide-se, ame-se, seja cliente Fancy Glam.</div>
+            <div class="Tela1texto"><input type="submit" value="COMPRE JÁ">  </div>
+        </div>
+        <div class="tela1Direita">
+            <img src="" alt="maquiagem tela1">
+        </div>
+    </div>
+
+    <div class="tela2">
+        <div class="Tela2Esquerda">
+            <div class="tela2imagem"><img src="" alt="maquiagem"></div>
+        </div>
+
+        <div class="Tela2Direita">
+            <div class="tela2titulo1">Sobre a empresa</div>
+            <div class="tela2titulo2">SEU BRILHO, SUA BELEZA, NOSSA <span id="tela2roxo">MISSÃO</span>.</div>
+            <div class="tela2-2col">
+                <div class="tela2-2colEsquerda">
+                    <div class="tela2-2colEsquerdaTitulo1"></div>
+                </div>
+
+                <div class="tela2-2colDireita">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
